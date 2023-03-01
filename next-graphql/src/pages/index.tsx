1762cd589555b05c56cb13bfef86118ec7db956e
Ruslan-Gubin <ruslan_gubin@rambler.ru>
2023-03-01 22:46:49 +0300

    commit22

--- next-graphql/src/pages/index.tsx
+++ next-graphql/src/pages/index.tsx
@@ -7,6 +7,8 @@ import { GET_CATEGORYES } from "../apps/apollo/CategoryRequest";
 import { ICategoryType, IProductType } from "../apps/types";
 import styles from "../apps/styles/pages/Home.module.scss";
 import { GET__MAXDISCOUNT__ALLPRODUCT, GET__MAXVIEWS__ALLPRODUCT, GET__NEW__ALLPRODUCT } from "../apps/apollo/productRequest/productRequest";
+import { useQuery } from "@apollo/client";
+import { LoaderShop } from "../shared";
 
 interface IHome {
   categoryData:  ICategoryType[] 
@@ -32,11 +34,26 @@ Error.getInitialProps = ({ res, err }) => {
 }
 
 export default function Home({ error , categoryData, maxWievsProducts, newProducts, maxDiscountProducts}:IHome) {
+  const {data: catData, loading: load1} = useQuery(GET_CATEGORYES)
+  const {data: viewsData, loading: load2} = useQuery(GET__MAXVIEWS__ALLPRODUCT,{
+    variables: {limit: 5}
+  })
+  const {data: newData, loading: load3} = useQuery(GET__NEW__ALLPRODUCT,{
+    variables: {limit: 5}
+  })
+  const {data: discoutData, loading: load4} = useQuery(GET__MAXDISCOUNT__ALLPRODUCT,{
+    variables: {limit: 5}
+  })
+
   const router = useRouter()
+  console.log(error);
+
 
 
   if (error) {
-    return <Error statusCode={error} />
+    // router.push('/lk/details')
+    // return <Error statusCode={error} />
+    console.log(error)
   }
    
       // if (!router.isFallback && !categoryData.length) {
@@ -46,62 +63,22 @@ export default function Home({ error , categoryData, maxWievsProducts, newProduc
 
   return (  
     <ShopLayout title="OnlineShop" keywords="Start project in home page">
-      {/* <section data-testid="test-root-home" className={styles.root}>
+      <section data-testid="test-root-home" className={styles.root}>
+       {load1 || load2 || load3 || load4 &&
+       <LoaderShop/>
+       }
+       
         <HomePage
-        categoryData={categoryData}
-        maxWievsProducts={maxWievsProducts}
-        newProducts={newProducts}
-        maxDiscountProducts={maxDiscountProducts}
+        categoryData={error ? catData : categoryData}
+        maxWievsProducts={error ? viewsData : maxWievsProducts}
+        newProducts={error ? newData : newProducts}
+        maxDiscountProducts={error ? discoutData : maxDiscountProducts}
         />
-      </section> */}
-      home
+      </section>
     </ShopLayout>
   );
 }
 
-// export const getServerSideProps = async ({req, query, }: NextPageContext) => {
-//   try {
-//     const { data: categoryData } = await client.query({
-//       query: GET_CATEGORYES,
-//     });
-//     const { data: maxWievsProducts } = await client.query({
-//       query: GET__MAXVIEWS__ALLPRODUCT,
-//       variables: {
-//         limit: 5,
-//       }
-//     });
-//     const { data: newProducts } = await client.query({
-//       query: GET__NEW__ALLPRODUCT,
-//       variables: {
-//         limit: 5,
-//       }
-//     });
-//     const { data: maxDiscountProducts } = await client.query({
-//       query: GET__MAXDISCOUNT__ALLPRODUCT,
-//       variables: {
-//         limit: 5,
-//       }
-//     });
-  
-//     return {
-//       props: {
-//         categoryData: categoryData.categorys,
-//         maxWievsProducts: maxWievsProducts.getMaxViewsProducts,
-//         newProducts: newProducts.getNewProducts,
-//         maxDiscountProducts: maxDiscountProducts.getMaxDiscountProducts, 
-//       },
-//     };
-//   } catch  {
-//     return {
-//       props: {
-//         categoryData: null,
-//         maxWievsProducts: null,
-//         newProducts: null,
-//         maxDiscountProducts: null, 
-//       },
-//     }
-//     } 
-// };
 export const getServerSideProps  = async ({req, query, }: NextPageContext) => {
   try {
     const { data: categoryData } = await client.query({
